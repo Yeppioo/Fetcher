0222e465b1f9a59c056f4791150f7ee0d1de7d4c
Yeppioo <yeppioo@qq.com>
2025-06-22 12:12:11 +0800

    feat(config): 添加友链页面路径配置
    - 在 config.js 文件中增加了 page 数组
    - 添加了多个友链页面路径，以适应不同的博客系统

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ const config = {
     linkPage: 'https://blog.yeppioo.vip/page/link',
     backLink: ['yeppioo.vip'],
     oldLink: ['yep.vin', 'daiyu.fun'],
+    page: ['links', 'link', 'links.html', 'friendlychain', 'youlian', 'site/link/', 'social/link/'],
+
 }
 
 module.exports = config;
