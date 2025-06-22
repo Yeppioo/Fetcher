86cd45c4073c04fb6c4cabeaed0e37c7a71f6afe
Yeppioo <yeppioo@qq.com>
2025-06-22 16:33:14 +0800

    feat(app): 添加更新时间功能
    - 引入 updateTime 模块
    - 在 app.js 中调用 updateTime 函数

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const friendChecker = require('./service/friend-checker');
+const updateTime = require('./service/update-time');
 
 friendChecker();
+updateTime();
---
+++ service/update-time.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+function updateTime() {
+    const updateTime = new Date().toISOString();
+    fs.writeFileSync(path.join(__dirname, '../api/static/data/time.txt'), updateTime);
+}
+
+module.exports = updateTime;
