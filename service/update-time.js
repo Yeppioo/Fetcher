4c8ee0162db4602304846e7c0fa48f2633d246c9
Yeppioo <yeppioo@qq.com>
2025-06-22 16:38:31 +0800

    fix(service): 修正时间更新服务中的文件路径
    - 将相对路径从当前目录修改为相对于 api/static/data/ 目录
    - 确保时间数据文件正确写入指定位置

--- service/update-time.js
+++ service/update-time.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function updateTime() {
     const updateTime = new Date().toISOString();
-    fs.writeFileSync(path.join(__dirname, './api/static/data/time.txt'), updateTime);
+    fs.writeFileSync(path.join(__dirname, '../api/static/data/time.txt'), updateTime);
 }
 
 module.exports = updateTime;
